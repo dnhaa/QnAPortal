fe3cf434b7fb57e34096b12f65aaf7b4e2964210
dnhaa <elnganha@gmail.com>
2022-05-20 22:25:11 +0700

    commit

---
+++ build/web/script/script.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//this function will be developed later
+function takelife(){
+    var life = document.getElementById('life').innerHTML;
+    console.log(life);
+    alert('We have claimed ' + life);
+}
+function erase(){
+    document.getElementById('life').innerHTML = 'You don\'t exist';
+}
+
+
+function downvote(){
+  document.getElementsByClassName('downvote')[0].classList.toggle('on');
+  if (document.getElementsByClassName('upvote')[0].classList.contains('on')){
+    document.getElementsByClassName('upvote')[0].classList.toggle('on');
+  }
+}
+function upvote(){
+  document.getElementsByClassName('upvote')[0].classList.toggle('on');
+  if ( document.getElementsByClassName('downvote')[0].classList.contains('on')){
+    document.getElementsByClassName('downvote')[0].classList.toggle('on');
+  }
+}
+  
